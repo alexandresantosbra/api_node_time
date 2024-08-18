e8d4531ca03870a7f2f0872155375382986b02f5
Alexandre Corrêagit config --global user.name Alexandre <verseries924@gmail.com>
2024-08-18 19:38:44 -0300

    start api node times banco mysql

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import routes from './routes.js'
+
+const app = express()
+
+// indicar para o express ler o body com json
+app.use(express.json())
+
+// usar o router
+app.use(routes)
+
+
+
+export default app
+
+
